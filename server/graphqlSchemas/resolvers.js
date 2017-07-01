a1f96370d91061cc822f631a55218c66691b79eb
ragavkumarv <ragavkumarv@gmail.com>
2017-07-01 11:47:26 +0530

    Intro to axios with users data from db.json

--- server/graphqlSchemas/resolvers.js
+++ server/graphqlSchemas/resolvers.js
@@ -1,15 +1,19 @@
-import { find, propEq } from 'ramda';
+// import { find, propEq } from 'ramda';
+import axios from 'axios';
 
-const users = [
-  { id: '1', firstName: 'Bill', age: 20 },
-  { id: '2', firstName: 'Alex', age: 40 },
-  { id: '3', firstName: 'Nick', age: 40 },
-];
+const AxiosPort = 4200;
+
+// const users = [
+//   { id: '1', firstName: 'Bill', age: 20 },
+//   { id: '2', firstName: 'Alex', age: 40 },
+//   { id: '3', firstName: 'Nick', age: 40 },
+// ];
 
 export default {
   Query: {
-    hello: (parent, args, _) => 'hiii',
-    user: (parent, args, _) => find(propEq('id', args.id))(users),
+    hello: (parent, args, context) => 'hiii',
+    user: (parent, { id }, context) => axios.get(`http://localhost:${AxiosPort}/users/${id}`)
+      .then(res => res.data),
   },
 };
 
