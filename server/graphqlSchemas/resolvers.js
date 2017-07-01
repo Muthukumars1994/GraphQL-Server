2b8bf43853e1d3716ec21a4706c916fe52576a8a
ragavkumarv <ragavkumarv@gmail.com>
2017-07-01 11:19:55 +0530

    User data configured

--- server/graphqlSchemas/resolvers.js
+++ server/graphqlSchemas/resolvers.js
@@ -1,15 +1,15 @@
 import { find, propEq } from 'ramda';
 
 const users = [
-  { id: '23', firstName: 'Bill', age: 20, companyId: '1' },
-  { id: '40', firstName: 'Alex', age: 40, companyId: '2' },
-  { id: '41', firstName: 'Nick', age: 40, companyId: '2' },
+  { id: '1', firstName: 'Bill', age: 20 },
+  { id: '2', firstName: 'Alex', age: 40 },
+  { id: '3', firstName: 'Nick', age: 40 },
 ];
 
 export default {
   Query: {
     hello: (parent, args, _) => 'hiii',
-    user: (root, args, _) => find(propEq('id', args.id))(users),
+    user: (parent, args, _) => find(propEq('id', args.id))(users),
   },
 };
 
