b522f134069de29468d8f16e5d195942a7dcf5d8
ragavkumarv <ragavkumarv@gmail.com>
2017-07-01 12:03:30 +0530

    Removed commented in resolvers and added company travesing from users

--- server/graphqlSchemas/myGraphQLSchema.js
+++ server/graphqlSchemas/myGraphQLSchema.js
@@ -20,8 +20,7 @@ const typeDefs = `
   type Query {
         hello: String! 
         user(id: String!): User
-        company(id: String!): Company
-    }
+       }
  `;
 
 const schema = makeExecutableSchema({
--- server/graphqlSchemas/resolvers.js
+++ server/graphqlSchemas/resolvers.js
@@ -1,18 +1,16 @@
-// import { find, propEq } from 'ramda';
 import axios from 'axios';
 
 const AxiosPort = 4200;
 
-// const users = [
-//   { id: '1', firstName: 'Bill', age: 20 },
-//   { id: '2', firstName: 'Alex', age: 40 },
-//   { id: '3', firstName: 'Nick', age: 40 },
-// ];
 
 export default {
   Query: {
     hello: (parent, args, context) => 'hiii',
     user: (parent, { id }, context) => axios.get(`http://localhost:${AxiosPort}/users/${id}`)
+      .then(res => res.data),
+  },
+  User: {
+    company: ({ companyId }, args, context) => axios.get(`http://localhost:${AxiosPort}/companies/${companyId}`)
       .then(res => res.data),
   },
 };
